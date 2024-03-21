cbbf80874cc19fc945ecd8bd72a5ff67caeac2f7
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-03-21 06:11:57 +0100

    Modules

---
+++ 07/roll.ml
@@ -0,0 +1,10 @@
+type 'a t = 'a * 'a list
+
+let empty = []
+
+let unit x = (x, empty)
+let cons x (y, u) = x, y :: u
+let hd (x, _) = x
+let tl (_, u) = match u with
+  | [] -> failwith "tl"
+  | x :: u -> (x, u)
